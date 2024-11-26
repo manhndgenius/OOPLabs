3d77d28da5017964fa926baa31340f7b62dd5711
Manh Nguyen <nguyenmanh1904ht@gmail.com>
2024-11-27 03:00:37 +0700

    .

---
+++ Lab03/AIMSProject/src/hust/soict/dsai/aims/Aims.java
@@ -0,0 +1,26 @@
+package hust.soict.dsai.aims;
+import hust.soict.dsai.aims.cart.Cart;
+import hust.soict.dsai.aims.disc.DigitalVideoDisc;
+
+public class Aims {
+	public static void main(String[] args) {
+		Cart anOrder = new Cart();
+		
+		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King","Animation","Roger Allers",87,19.95f);
+		anOrder.add_DigitalVideoDisc(dvd1);
+		
+		DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars","Science Fiction","George Lucas",87,24.95f);
+		anOrder.add_DigitalVideoDisc(dvd2);
+		
+		DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin","Ainmation",18.99f);
+		anOrder.add_DigitalVideoDisc(dvd3);
+		
+		System.out.println("The Cost is: ");
+		System.out.println(anOrder.totalCost());
+		
+		anOrder.remove_DigitalVideoDisc(dvd3);
+
+		System.out.println("The remain cost is: ");
+		System.out.println(anOrder.totalCost());
+	}
+}
